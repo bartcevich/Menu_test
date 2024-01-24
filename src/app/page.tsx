853ea3708ab520939fb7d5d836536fa6544e75ad
Andrei(@elektrik) <101565487+bartcevich@users.noreply.github.com>
2024-01-24 12:59:30 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,23 @@ import MenuGroupsOpen from "@/components/MenuGroupsOpen";
 import { MenuProvider } from "@/context/IngredientsContext";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
+import bgImage from "@/assets/images/background3.jpg";
 
 export default function Home() {
   return (
     <Provider store={store}>
       {/* <Form/> */}
-      <AllIngredients />
+      <div
+        style={{
+          backgroundImage: `url(${bgImage.src})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+          minHeight: "100vh",
+        }}
+      >
+        <AllIngredients />
+      </div>
       {/* <MenuGroupsOpen /> */}
       {/* <MenuGroups /> */}
       {/* <Dinner/> */}
